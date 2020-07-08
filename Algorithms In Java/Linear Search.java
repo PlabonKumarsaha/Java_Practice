4dcc2c37400273c5f58a0be61e2ac08156406722
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2020-07-08 19:45:45 +0600

    Update Linear Search.java

--- Algorithms In Java/Linear Search.java
+++ Algorithms In Java/Linear Search.java
@@ -1,37 +1,44 @@
+package searching;
+
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
-class GFG
-{
-    
-   
-    public static int search(int arr[], int x)
-    {
-        int n = arr.length;
-        for(int i = 0; i < n; i++)
-        {
-            if(arr[i] == x)
-                return i;
+/**
+ *
+ * @author PKS
+ */
+public class Searching {
+
+    int linearSearch (int arr[],int num){
+        
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]==num){
+                return i+1;
+            }
         }
         return -1;
     }
-
-    public static void main(String args[])
-    {
-
-        Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
-        int arr[] = { 2, 3, 4, 10, 40 };
-
-        for(int i = 0;i<num;i++)
-        {
-          arr[i]=input.nextInt();
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Searching sear = new Searching();
+        Scanner sc = new Scanner(System.in);
+        
+        int arr[] = new int[50];
+        
+        for(int i = 0;i<10;i++){
+            
+            arr[i]= sc.nextInt();
         }
-        System.out.println("Enter input");
-        int x = input.nextInt();
-        int result = search(arr, x);
-        if(result == -1)
-            System.out.print("Element is not present in array");
-        else
-            System.out.print("Element is present at index  " + (result+1));
+        
+        int input;
+        
+        input = sc.nextInt();
+        sear.linearSearch(arr, input);
+        //linearSearch(input);
+        
     }
-} 
+    
+}
+
